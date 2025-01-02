490a0efd54739fcf258d89bfc09bc192a85bf131
ericleonen <ericleonen@gmail.com>
2025-01-01 17:15:58 -0800

    update comments

--- geometry/number.ts
+++ geometry/number.ts
@@ -10,6 +10,10 @@ export function isBetween(x: number, interval: [number, number]): boolean {
     }
 }
 
+/**
+ * Returns true if x is within the given interval (endpoints inclusive), false otheriwse. Can
+ * handle improper intervals (the larger value is the first value).
+ */
 export function isBetweenWorklet(x: number, interval: [number, number]): boolean {
     "worklet";
     if (interval[0] <= interval[1]) {
@@ -19,6 +23,10 @@ export function isBetweenWorklet(x: number, interval: [number, number]): boolean
     }
 }
 
+/**
+ * Clamps the given number in the given interval. Can handle improper intervals (the larger
+ * value is the first value).
+ */
 export function clamp(x: number, interval: [number, number]): number {
     if (interval[0] <= interval[1]) {
         return Math.max(Math.min(x, interval[1]), interval[0]);
@@ -27,6 +35,10 @@ export function clamp(x: number, interval: [number, number]): number {
     }
 }
 
+/**
+ * Clamps the given number in the given interval. Can handle improper intervals (the larger
+ * value is the first value).
+ */
 export function clampWorklet(x: number, interval: [number, number]): number {
     "worklet";
     if (interval[0] <= interval[1]) {
@@ -36,10 +48,16 @@ export function clampWorklet(x: number, interval: [number, number]): number {
     }
 }
 
+/**
+ * Returns true if the given numbers are within tolerance (default 0.01) of eachother. 
+ */
 export function isApproximatelyEqual(x1: number, x2: number, tolerance = 0.01) {
     return Math.abs(x1 - x2) <= tolerance;
 }
 
+/**
+ * Returns true if the given numbers are within tolerance (default 0.01) of eachother. 
+ */
 export function isApproximatelyEqualWorklet(x1: number, x2: number, tolerance = 0.01) {
     "worklet";
     return Math.abs(x1 - x2) <= tolerance;
